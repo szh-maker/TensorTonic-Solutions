72dfb7ed460a45eb4298f23ee5574177499f43a1
szh-maker <55565127+szh-maker@users.noreply.github.com>
2026-03-31 21:27:59 +0800

    Add solution for normalize-3d

---
+++ normalize-3d/normalize-3d.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def normalize_3d(v):
+    """
+    Normalize 3D vector(s) to unit length.
+    """
+    # Your code here
+    v = np.array(v, dtype = float)
+    norm = np.linalg.norm(v, axis=-1, keepdims=True)
+    norm = np.where(norm == 0, 1, norm)
+    return v / norm
