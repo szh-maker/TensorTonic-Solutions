ef9600e995013f254316e1eb57c85dbddb56fd7b
szh-maker <55565127+szh-maker@users.noreply.github.com>
2026-04-18 10:30:12 +0800

    Add solution for numpy-arange-linspace

---
+++ study-plans/numpy-basics/numpy-arange-linspace/numpy-arange-linspace.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def create_sequence(start, stop, param, kind):
+    """
+    Returns: 1D ndarray of float64 values
+    """
+    if kind == 'arange':
+        return np.arange(start, stop, param, dtype=np.float64)
+    elif kind == 'linspace':
+        return np.linspace(start, stop, param, dtype=np.float64)
